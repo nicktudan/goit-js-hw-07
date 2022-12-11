31f060fd394f9df049c1a34d35f97f379eabe998
Olga Pozdniakova <nicktudan@gmail.com>
2022-12-12 00:03:16 +0100

    Done task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,18 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems.reduce((acc, {
+    preview,
+    original,
+    description
+}) => acc + `<a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`, '');
+
+gallery.insertAdjacentHTML('beforeend', markup);
+
+var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
